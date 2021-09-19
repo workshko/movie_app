b73a9eb0acdc3cce2476d38edebb5847e5216828
KoTank <work.shko@gmail.com>
2021-09-19 17:47:03 +0900

    jsx 개념 및 prop 연습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+
+function Food({favorite}){
+  console.log(favorite);
+
+  return <h1>I like {favorite}</h1>;
+}
+
 function App() {
   return (
     <div className="App">
     <h1>hello React!!</h1>
+    <Food favorite="kimchi"/>
+    <Food favorite="ramen"/>
+    <Food favorite="sanmgiosal"/>
+    <Food favorite="사랑해"/>
     </div>
   );
 }
---
+++ src/Potato.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Potato(){
+    return <h3>I Love Potato</h3>;
+    
+}
+
+export default Potato;
