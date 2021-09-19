ecd2f3f1c378274d672c393c2d6976570c34224e
KoTank <work.shko@gmail.com>
2021-09-19 19:37:45 +0900

    React 컴포넌트 라이프사이클 실습

--- src/App.js
+++ src/App.js
@@ -1,57 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = 
-[
-   {
-      id : 1
-     ,name : "김치"
-     ,image : "https://contents.lotteon.com/itemimage/_v203726/LE/12/05/06/12/42/_1/22/75/97/45/0/LE1205061242_1227597450_1.jpg/dims/resizef/554X554"
-     ,rating : 3.8
-   }
-  ,{
-      id : 2
-     ,name : "라면"
-     ,image : "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg"
-     ,rating : 4.8
-  }
-  ,{
-      id : 3
-     ,name : "김밥"
-     ,image : "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg"
-     ,rating : 4.1
-  }
-  ,{
-      id : 4
-     ,name : "치킨"
-     ,image : "https://blog.kakaocdn.net/dn/eR0RoG/btqXY1vmW8J/cKhyjxeV9wAZK79wWcxZc0/img.png"
-     ,rating : 5.0
-  }
-];
+//this.setState 를 호출 할 때마다, React는 render() 함수를 호출한다! << 반드시 기억!!
+class App extends React.Component{
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-;
-function Food({name, picture, rating}){
-  
-  return <div>
-    <h1>I like {name}</h1>
-    <h3>{rating}/5.0</h3>
-    <img src={picture} alt={name} width="250px" height="250px"></img>
-  </div>;
-}
+  constructor(props){
+    super(props);
+    console.log("생성자");
+  }
 
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish => (
-        <Food key ={dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>
-      ))}
-    </div>
-  );
+  state = {
+    count: 0
+  };
+
+  add = () => {
+    console.log("Add");
+    this.setState( current => ( { count : current.count + 1}));
+  };
+
+  minus = () => {
+    console.log("minus");
+    this.setState( current => ( { count : current.count - 1}));
+  };
+
+  componentDidMount(){
+    console.log("컴포넌트가 렌더링 됬다.");
+  }
+
+  componentDidUpdate(){
+    console.log("컴포넌트가 업데이트 됬다.");
+  }
+
+  componentWillUnmount(){
+    console.log("다른 페이지로 가기 전 호출된다!");
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>The numver is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+
+  }
+
 }
 
 export default App;
