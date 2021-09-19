d81bc3049af92b30bd7b304b92ee6680eca9d17a
KoTank <work.shko@gmail.com>
2021-09-19 18:55:42 +0900

    npm 설치, prop-types 실습

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import React from "react";
-
-function Food({name, picture}){
-  
-  return <div>
-    <h1>I like {name}</h1>
-    <img src={picture} width="250px" height="250px"></img>
-  </div>;
-}
+import PropTypes from "prop-types";
 
 const foodILike = 
 [
    {
-      name : "김치"
+      id : 1
+     ,name : "김치"
      ,image : "https://contents.lotteon.com/itemimage/_v203726/LE/12/05/06/12/42/_1/22/75/97/45/0/LE1205061242_1227597450_1.jpg/dims/resizef/554X554"
+     ,rating : 3.8
    }
   ,{
-      name : "라면"
+      id : 2
+     ,name : "라면"
      ,image : "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg"
+     ,rating : 4.8
   }
   ,{
-      name : "김밥"
+      id : 3
+     ,name : "김밥"
      ,image : "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg"
+     ,rating : 4.1
   }
   ,{
-      name : "치킨"
+      id : 4
+     ,name : "치킨"
      ,image : "https://blog.kakaocdn.net/dn/eR0RoG/btqXY1vmW8J/cKhyjxeV9wAZK79wWcxZc0/img.png"
+     ,rating : 5.0
   }
 ];
 
-//동적 데이터 바인딩 방법 실습
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+}
+;
+function Food({name, picture, rating}){
+  
+  return <div>
+    <h1>I like {name}</h1>
+    <h3>{rating}/5.0</h3>
+    <img src={picture} alt={name} width="250px" height="250px"></img>
+  </div>;
+}
 
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name = {dish.name} picture = {dish.image}/>
+        <Food key ={dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>
       ))}
     </div>
   );
